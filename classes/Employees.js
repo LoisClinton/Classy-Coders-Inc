19ab6ed8a2cde1a81b2cf446d2151ce72889060c
LoisClinton <loisclinton@hotmail.co.uk>
2023-10-22 09:03:50 +0100

    created employees, created Manager, created Software engineer

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,10 +1,38 @@
 class Employees {
+  #salary;
+  #isHired;
 
+  constructor(name, position, salary) {
+    this.name = name;
+    this.position = position;
+    this.#salary = salary;
+    this.#isHired = true;
+  }
+  getSalary() {
+    return this.#salary;
+  }
+  setSalary(amount) {
+    this.#salary = amount;
+  }
+  getStatus() {
+    return this.#isHired;
+  }
+  setStatus(command) {
+    switch (command) {
+      case "hire":
+        this.#isHired = true;
+        break;
 
+      case "fire":
+        this.#isHired = false;
+        break;
 
-    
+      default:
+        break;
+    }
+  }
 }
 
 module.exports = {
-    Employees,
-}
+  Employees,
+};
